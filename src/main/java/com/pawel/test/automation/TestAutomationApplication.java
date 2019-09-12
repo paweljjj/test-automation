69614e027618131dd59083c26f11d371e0d72b34
paweljjj <pawel.juchimiuk@gmail.com>
2019-09-12 16:55:15 +0200

    initialize spring boot project

---
+++ src/main/java/com/pawel/test/automation/TestAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.pawel.test.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAutomationApplication.class, args);
+	}
+
+}
