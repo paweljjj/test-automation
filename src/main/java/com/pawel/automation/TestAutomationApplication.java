5b4bf165352f250bfd066478cdcaa9fdee26d1aa
paweljjj <pawel.juchimiuk@gmail.com>
2019-09-12 17:27:06 +0200

    add cucumber

--- src/main/java/com/pawel/automation/TestAutomationApplication.java
+++ src/main/java/com/pawel/automation/TestAutomationApplication.java
@@ -1,4 +1,4 @@
-package com.pawel.test.automation;
+package com.pawel.automation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/test/resources/features/selenium.feature
@@ -0,0 +1,6 @@
+Feature: Selenium tests with automationpractice.com
+
+    Scenario: Example selenium test
+    Given The browser is opened
+    When I enter url
+    Then The page is opened
